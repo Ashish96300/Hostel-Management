acd9661dfeff6728655ebdef1c899a0c2ccfc0cb
Ashish Sen <ashishsen2609@gmail.com>
2026-05-14 09:38:03 +0530

    store fix

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,21 +1,53 @@
+// import { create } from "zustand";
+
+// const useAuthStore = create((set) => ({
+//     user: null,
+//     isAuthenticated: false,
+//     isLoading: true,
+
+//     setAuth: (userData) => set({ 
+//         user: userData, 
+//         isAuthenticated: !!userData, 
+//         isLoading: false 
+//     }),
+
+//     logout: () => set({ 
+//         user: null, 
+//         isAuthenticated: false, 
+//         isLoading: false 
+//     }),
+// }));
+
+// export default useAuthStore;
+
 import { create } from "zustand";
 
 const useAuthStore = create((set) => ({
-    user: null,
-    isAuthenticated: false,
+    user: JSON.parse(localStorage.getItem("user")) || null,
+    isAuthenticated: !!localStorage.getItem("user"),
     isLoading: true,
 
-    setAuth: (userData) => set({ 
-        user: userData, 
-        isAuthenticated: !!userData, 
-        isLoading: false 
-    }),
+    setAuth: (userData) => {
+        if (userData) {
+            localStorage.setItem("user", JSON.stringify(userData));
+        } else {
+            localStorage.removeItem("user");
+        }
+        set({ 
+            user: userData, 
+            isAuthenticated: !!userData, 
+            isLoading: false 
+        });
+    },
 
-    logout: () => set({ 
-        user: null, 
-        isAuthenticated: false, 
-        isLoading: false 
-    }),
+    logout: () => {
+        localStorage.removeItem("user");
+        set({ 
+            user: null, 
+            isAuthenticated: false, 
+            isLoading: false 
+        });
+    },
 }));
 
 export default useAuthStore;
