ac558639088b8ae679b8ccb938c4ff91546b76af
Ashish Sen <ashishsen2609@gmail.com>
2026-05-14 06:50:50 +0530

    error in frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL ||import.meta.env.VITE_API_URL + "/api/v1";
+
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/v1", 
+    baseURL:BASE_URL, 
     withCredentials: true,
 });
 
