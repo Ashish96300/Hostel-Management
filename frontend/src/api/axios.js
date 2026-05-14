b8a1698b0e80f500133c2b4db508849242ea2086
Ashish Sen <ashishsen2609@gmail.com>
2026-05-14 07:44:13 +0530

    last

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const BASE_URL =import.meta.env.VITE_API_URL + "/api/v1";
 
+if (!import.meta.env.VITE_API_URL) {
+  console.error("❌ VITE_API_URL is not set! Check your Vercel env variables.");
+}
+
 const api = axios.create({
     baseURL:BASE_URL, 
     withCredentials: true,
