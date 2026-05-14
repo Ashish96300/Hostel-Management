7f8153ef8f7d363a751fe977075bb8f2f04f1b41
Ashish Sen <ashishsen2609@gmail.com>
2026-05-14 07:40:43 +0530

    cors check

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,9 +9,9 @@ const app = express();
 
 app.use(cors({
     origin: [
+        process.env.CORS_ORIGIN,
         "http://localhost:5173",  
         "http://localhost:3000",  
-        process.env.CORS_ORIGIN, 
     ].filter(Boolean),
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
